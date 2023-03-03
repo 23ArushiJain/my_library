2446e1cc146b7373f4693bdbf5af7ff67f22323b
23ArushiJain <87538941+23ArushiJain@users.noreply.github.com>
2023-03-03 07:53:28 +0530

    Update sonarBuild.groovy

--- vars/sonarBuild.groovy
+++ vars/sonarBuild.groovy
@@ -1,5 +1,5 @@
 def call(Map config) {
-  stage('Sonar') {
+  stage('sonarBuild') {
     withSonarQubeEnv('sonarqube') {
       sh "mvn sonar:sonar -Dsonar.projectKey=${config.projectKey} -Dsonar.projectName=${config.projectName} -Dsonar.projectVersion=${config.projectVersion}"
     }
