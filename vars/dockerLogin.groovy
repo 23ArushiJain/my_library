387fa3a857d6e408a78b69edc9e89b464e3fbf1d
23ArushiJain <87538941+23ArushiJain@users.noreply.github.com>
2023-03-03 15:47:59 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,5 +1,5 @@
  def call(Map config = [:]) {
-    sh "docker image build -t ${config.hubUser}/${config.project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    sh "docker image build -t ${config.image}/${config.tag}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
     withCredentials([usernamePassword(
             credentialsId: "doc",
             usernameVariable: "USER",
